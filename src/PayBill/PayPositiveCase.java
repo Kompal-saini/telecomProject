e21ae1504a38ff7b90d39de6987aa1d7c9e7bf76
kompal-saini <kompalsaini24@gmail.com>
2024-05-02 14:35:21 -0400

    modified positiveCase class

--- src/PayBill/PayPositiveCase.java
+++ src/PayBill/PayPositiveCase.java
@@ -6,22 +6,21 @@ import AddCustomer.AddPositiveCase;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class PayPositiveCase extends AddPositiveCase {
-	
-	  static String customerID;
-	public static void billPayment(){
+	  
+	public static void billPayment(String custID){
 
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("http://demo.guru99.com/telecom/billing.php");
 
-		driver.findElement(By.name("customer_id")).sendKeys(customerID);
+		driver.findElement(By.name("customer_id")).sendKeys(custID);
 		driver.findElement(By.name("submit")).click(); 
 	}
 	
 	public static void main(String[] args) {
 		
-		customerID =  AddPositiveCase.addCustomer();
-		billPayment();
+		String customerID =  AddPositiveCase.addCustomer();
+		billPayment(customerID);
 
 	}
 
